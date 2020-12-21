88aaaefba309295a1a10c69354933967ff722779
Kevin Barreto <barretokevin@hotmail.com>
2020-12-20 19:11:55 -0600

    Numbers and its methods

---
+++ numbers/numbers.js
@@ -0,0 +1,19 @@
+// Addition
+50 + 10;
+//Subtraction
+20 - 10;
+//Multiplication
+20 * 8;
+// Division
+100 / 5;
+// Modulo Operator
+10 % 2; //output is 0
+// Exponential
+console.log(3 ** 2);
+
+// NaN (Not a Number)
+console.log(0 / 0);
+console.log(NaN / 2);
+
+// Infinity
+console.log(1 / 0);
