d9d37dfa8f4d44fa3f901ec88030d35f8e93432c
Kevin Barreto <barretokevin@hotmail.com>
2020-12-20 19:11:12 -0600

    All about variables

---
+++ variables/variables.js
@@ -0,0 +1,24 @@
+// Let Keyword
+let hens = 4;
+let roosters = 19;
+console.log(hens); // output is 4
+hens = hens + 10;
+console.log(hens); // output is 14
+
+// Unary Operators
+let score = 8;
+score += 2;
+console.log(score); // output is 10
+
+score -= 5;
+console.log(score); // output is 5
+
+score *= 3;
+console.log(score); // output is 15
+
+score++;
+console.log(score); // outuput is 16
+
+// Const Keyword (cannot be changed once set)
+const age = 28;
+age = 30; // ERROR!
